536a3967214f01216f5c3b3db2faee87ebb7318b
Chrystel <chrystelbardsley@gmail.com>
2017-08-23 13:46:27 -0600

    Begining of scripts in Forest

---
+++ Japanese Forest/Assets/Trigger_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_script : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+        print("Welcome!")
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit () {
+        print("Be back soon!")
+		
+	}
+}
