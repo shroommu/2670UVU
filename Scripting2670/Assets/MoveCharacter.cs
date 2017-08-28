fc8a190a27e9904f7ce43dc0c30c42b8064527db
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-08-28 14:21:50 -0600

    updated scripts in TriggerEvents

---
+++ Scripting2670/Assets/MoveCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+    CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+        MoveInput.KeyAction = Move;
+	}
+
+    void Move (float _movement)
+    {
+        print(_movement);
+    }
+}
---
+++ Scripting2670/Assets/MoveInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveInput : MonoBehaviour {
+
+    public static Action<float> KeyAction;
+
+    private void Update()
+    {
+        if (KeyAction != null)
+        {
+            KeyAction(Input.GetAxis("Horizontal"));
+        }
+    }
+}
