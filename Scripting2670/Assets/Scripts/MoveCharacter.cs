c476fd61aff2885801a863089f6b3d15af101ecd
Anthony Romrell <anthonyromrell@mac.com>
2017-08-28 14:05:18 -0600

    Adding Character Move Action Call

---
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+	Vector3 tempMove;
+    public float speed = 5;
+
+    void Start () {
+		cc = GetComponent<CharacterController>();
+		MoveInput.KeyAction = Move;
+	}
+
+	void Move (float _movement) {
+		tempMove.x = _movement*speed*Time.deltaTime;
+		cc.Move(tempMove);
+	}
+}
