19fe46ad36115a23ec6514ed9884ab1039293adf
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-09-05 13:21:40 -0600

    Revert "designed first level for game design assignment"
    This reverts commit ca6457721a6cf575702c260b4889aa78927d2c6f.

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class MoveCharacter : MonoBehaviour {
 
@@ -10,52 +11,25 @@ public class MoveCharacter : MonoBehaviour {
     public float speed = 5;
     public float gravity = 1;
     public float jumpHeight = 0.2f;
-	public int jumpNum = 0;
 
     void Start () {
 		cc = GetComponent<CharacterController>();
 		PlayButton.Play += OnPlay;
 	}
 
-	void Update() {
-		//checks for ground and resets jump count to allow another jump
-		if (cc.isGrounded && jumpNum > 0){
-			jumpNum = 0;
-		}
-	}
-	//enables inputs once play button pressed
 	void OnPlay () {
 		MoveInput.JumpAction = Jump;
 		MoveInput.KeyAction += Move;
 		PlayButton.Play -= OnPlay;
-		//tempMove.eulerAngles.y = Mathf.Clamp(transform.rotation.y, -30, 30);
 	}
-	
+
 	void Jump () {
-		//increments jump count var, performs jump
-		if (jumpNum < 2){
-			++jumpNum;
-			tempMove.y = jumpHeight;
-		}
+		tempMove.y = jumpHeight;
 	}
-	//moves character horizontally
+
 	void Move (float _movement) {
 		tempMove.y -= gravity*Time.deltaTime;
 		tempMove.x = _movement*speed*Time.deltaTime;
 		cc.Move(tempMove);
-	}
-	//makes speed var accessible to other scripts
-	public float GetSpeed(){
-		return speed;
-	}
-
-	//makes player position accessible to other scripts
-	public float GetPosX(){
-		return tempMove.x;
-	}
-
-	//makes player position accessible to other scripts
-	public float GetPosY(){
-		return tempMove.y;
 	}
 }
