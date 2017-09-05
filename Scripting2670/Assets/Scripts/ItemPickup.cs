9ed03381d3385fcc26ddbef1548a8af463e51f6d
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-09-05 13:21:07 -0600

    added unhide script, broke play button

---
+++ Scripting2670/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+
+	public GameObject pickupObj;
+
+	// Use this for initialization
+	void Start () {
+		PlayButton.Play += OnPlay;
+	}
+
+	void OnPlay () {
+		PlayButton.Play -= OnPlay;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other)
+	{
+		print("Item Collected");
+		Destroy(pickupObj);
+	}
+		
+	
+}
---
+++ Scripting2670/Assets/UnhideArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnhideArea : MonoBehaviour {
+
+	public GameObject coverObj;
+
+	void Start () {
+		PlayButton.Play += OnPlay;
+	}
+
+	void OnPlay () {
+		PlayButton.Play -= OnPlay;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		coverObj.SetActive(false);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		coverObj.SetActive(true);
+	}
+}
