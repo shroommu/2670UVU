fb31c317f9c238652dbbc9862f0e3432dfa92131
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-09-12 12:52:47 -0600

    added patrolling enemy script

---
+++ Scripting2670/Assets/Scripts/EnemyPatrolX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolX : MonoBehaviour {
+
+	public Transform enemy;
+	public float enemySpeed;
+
+	void Update(){
+		enemy.transform.position += new Vector3(enemySpeed*Time.deltaTime, 0, 0);
+	}
+
+	void OnTriggerEnter () {
+		enemySpeed = -enemySpeed;
+		print("Hit Trigger");
+	}
+
+}
---
+++ Scripting2670/Assets/Scripts/EnemyPatrolY.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolY : MonoBehaviour {
+
+	public Transform enemy;
+	public float enemySpeed;
+
+	void Update(){
+		enemy.transform.position += new Vector3(0, enemySpeed*Time.deltaTime, 0);
+	}
+
+	void OnTriggerEnter () {
+		enemySpeed = -enemySpeed;
+		print("Hit Trigger");
+	}
+
+}
