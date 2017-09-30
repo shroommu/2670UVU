b9488117daf9f876c79cf257dddfd27c240b0584
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-09-30 09:45:26 -0600

    added navmesh

---
+++ Scripting2670/Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+	public NavMeshAgent agent;
+	public Transform player;
+
+	void Update () {
+		agent.destination = player.position;		
+	}
+}
