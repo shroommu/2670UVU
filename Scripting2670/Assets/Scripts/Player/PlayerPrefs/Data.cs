5e394af2d66e8aea8b26352e8a1968cf3df2430d
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-10-13 14:18:00 -0600

    added scripts from LearningAssets/PlayerPrefs

---
+++ Scripting2670/Assets/Scripts/Player/PlayerPrefs/Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Data {
+
+	public int totalScore;
+	public float health;
+	public string playerName;
+	public Vector3 checkPoint;
+	public int gold;
+
+}
---
+++ Scripting2670/Assets/Scripts/Store/UpdateGold.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGold : MonoBehaviour {
+
+	public Text goldUI;
+
+	void Awake (){
+		GetAndSetGameData.UpdateGold += GoldHandler;
+		goldUI.GetComponent<Text>();
+	}
+
+	void GoldHandler(int _gold){
+		
+	}
+}
