5d73ac76e0a8ca8d6579b3d85f15d92a058ebf40
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2017-12-19 15:45:37 -0700

    started work on temple

--- Scripting2670/Assets/Scripts/Environmental/Teleport.cs
+++ Scripting2670/Assets/Scripts/Environmental/Teleport.cs
@@ -8,27 +8,21 @@ public class Teleport : MonoBehaviour {
 	public static Action FreezeControls;
 	public static Action UnfreezeControls;
 
-	public GameObject gameOverBlack;
-
 	public Transform teleportPoint;
 	public Transform player;
 
 	private bool teleporting = false;
 
-	void OnTriggerEnter(Collider other){
-		if(other.tag == "Player"){
-			teleporting = true;
-			StartCoroutine("TeleportThis");
-		}
+	void OnTriggerEnter(){
+		teleporting = true;
+		StartCoroutine(TeleportThis());
 	}
 
 	IEnumerator TeleportThis () {
 		while(teleporting){
 			FreezeControls();
-			gameOverBlack.SetActive(true);
 			player.transform.position = teleportPoint.transform.position;
 			yield return new WaitForSeconds(1);
-			gameOverBlack.SetActive(false);
 			UnfreezeControls();
 			teleporting = false;
 		}
