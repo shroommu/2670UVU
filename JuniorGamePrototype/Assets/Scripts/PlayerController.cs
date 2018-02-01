5bc4238b560eca02c3cca57ee629b2fd928b20e8
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2018-01-31 18:24:14 -0700

    Imported a couple scripts

--- JuniorGamePrototype/Assets/Scripts/PlayerController.cs
+++ JuniorGamePrototype/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,16 @@ public class PlayerController : MonoBehaviour
 	private float verticalVelocity = 0.0f;
 	private float speed = 0;
 	private Vector3 move = Vector3.zero;
-	private Vector3 rotation = Vector3.zero;
-	private Vector3 camRotation = Vector3.zero;
-
-	private Camera camera;
 
 	void Start () 
 	{
 		cc = GetComponent<CharacterController> ();
-		camera = GetComponentInChildren<Camera> ();
-		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	void Update () 
 	{
+		if(Time.timeScale ==  1)
+		{
 		if (cc.isGrounded) 
 		{
 			verticalVelocity = player.Jump (verticalVelocity);
@@ -31,27 +27,16 @@ public class PlayerController : MonoBehaviour
 		{
 			verticalVelocity = player.ApplyGravity (verticalVelocity);
 		}
-
-	
+			
 		speed = player.Run (speed);
-
-		//rotation = transform.rotation.eulerAngles;
-
-		rotation.y = Input.GetAxis ("Mouse X");
-		camRotation.x = Input.GetAxis ("Mouse Y");
-
-
-		camera.transform.Rotate (-camRotation);//the negative will need to become an option because matt likes inverted look
 
 		move = Vector3.zero;
 		move.x = Input.GetAxis ("Horizontal") * speed;
 		move.z = Input.GetAxis ("Vertical") * speed;
 		move.y = verticalVelocity;
 
-		cc.transform.position += transform.forward * speed * Time.deltaTime;
-		//transform.rotation = Quaternion.Euler (rotation);
-
-		cc.transform.Rotate (0, rotation.y, 0);
-		Camera.main.transform.localRotation = Quaternion.Euler(rotation.z, 0, 0);
+		move = transform.TransformDirection (move);
+		cc.Move (move * Time.deltaTime);
+		}
 	}
 }
