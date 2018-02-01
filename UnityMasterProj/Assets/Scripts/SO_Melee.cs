52d162689dd8bb83e7c6f1da5c79b08ae4883614
Matthew Meyers <jedimastermeyers@gmail.com>
2018-02-01 14:47:47 -0700

    forgot to stage file

---
+++ UnityMasterProj/Assets/Scripts/SO_Melee.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Recovery", menuName = "Scriptable Objects/Abilities/Melee Attack")]
+public class SO_Melee : ABS_Abilities
+{
+	public float damage;
+	public GameObject weaponAsset;
+
+	public override void UseAbility(string _triggerName, Animator _anim)
+	{
+		_anim.SetTrigger(_triggerName);
+	}
+}
