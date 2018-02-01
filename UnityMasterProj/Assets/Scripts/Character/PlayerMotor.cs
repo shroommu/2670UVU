37af6555fd66da9e44351496492e6fd55eb98c75
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2018-02-01 14:47:06 -0700

    integrated health, knockback, shooting, enemies, character respawn into master proj

---
+++ UnityMasterProj/Assets/Scripts/Character/PlayerMotor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    public LayerMask groundLayers;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    [SerializeField]
+    private float fallMultiplier = 2.5f;
+
+    private Rigidbody rb;
+    private CapsuleCollider col;
+   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    //Gets a movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Gets a rotational vector
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+
+    }
+
+
+    //Run Every Physics Iteration
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+
+        if(rb.velocity.y<0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier -1) *Time.deltaTime;
+        }
+    }
+
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity !=Vector3.zero)
+        {
+            rb.MovePosition(rb.position+velocity*Time.fixedDeltaTime);
+        }
+    }
+
+    //Executes thed rotation that has been calculated.
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation*Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+    
+    //Gets a rotational vector for the camera  
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    public void PlayerJump(float _jumpSpeed)
+    {
+        Vector3 up = transform.TransformDirection(Vector3.up);
+        rb.AddForce(up * _jumpSpeed, ForceMode.Impulse);
+    }
+
+    public bool isGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+}
---
+++ UnityMasterProj/Assets/Scripts/Character/SpawnCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject[] playerSpawnLocation;
+
+	// Use this for initialization
+	void Start () {
+		player.transform.position = playerSpawnLocation[Random.Range(0, playerSpawnLocation.Length)].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown ("q")) {
+			spawnPlayer ();
+		}
+	}
+
+	public void spawnPlayer ()
+	{
+		player.transform.position = playerSpawnLocation[Random.Range(0, playerSpawnLocation.Length)].transform.position;
+		player.transform.rotation = playerSpawnLocation [Random.Range (0, playerSpawnLocation.Length)].transform.rotation;
+	}
+}
+
