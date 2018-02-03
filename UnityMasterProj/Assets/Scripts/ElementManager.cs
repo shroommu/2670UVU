10d80a3ddd26b22ffaf99cda2fc8557c662175b3
Matthew Meyers <jedimastermeyers@gmail.com>
2018-02-03 14:36:08 -0700

    Added collections

---
+++ UnityMasterProj/Assets/Scripts/ElementManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ElementManager : MonoBehaviour {
+	public SO_Player player;
+	private Animal animal;
+	public Text elementTxt;
+	public Image symbol0;
+	public Image symbol1;
+
+	void Start(){
+		player.animals.Clear ();
+		player.currentElement = Elemental.elementType.Neutral;
+		elementTxt = GameObject.Find("Element_Display_Text").GetComponent<Text>();
+		symbol0 = GameObject.Find("ElementImage0").GetComponent<Image>();
+		symbol1 = GameObject.Find("ElementImage1").GetComponent<Image>();
+		player.currentAnimal = null;
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			if (player.animals.Count == 2) {
+				SwapElements ();
+			}
+		}
+	}
+	void SwapElements(){
+		GameObject _temp = player.animals [1];
+		player.animals [1] = player.animals [0];
+
+		player.animals [0] = _temp;
+		player.currentAnimal = _temp;
+
+		player.animals [1].GetComponent<AnimalMove> ().animalMoveState = AnimalMove.moveState.Pose1;
+		player.animals [0].GetComponent<AnimalMove>().animalMoveState = AnimalMove.moveState.Pose0;
+		elementTxt.text = "" + player.animals[0].GetComponent<AnimalBehavior>().animal.type;
+		player.currentElement = player.animals[0].GetComponent<AnimalBehavior>().animal.type;
+		symbol0.sprite = player.animals[0].GetComponent<AnimalBehavior>().animal.symbol;
+		symbol1.sprite = player.animals[1].GetComponent<AnimalBehavior>().animal.symbol;
+	}
+}
---
+++ UnityMasterProj/Assets/Scripts/MeteorCrash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorCrash : MonoBehaviour {
+	public Transform target;
+	public List <GameObject> animals;
+	public Rigidbody body;
+	public float speed; 
+	void Start () {
+		target = GameObject.Find ("Meteor_Target").transform;
+		body= GetComponent<Rigidbody> ();
+		transform.LookAt (target);
+	}
+	void Update(){
+		body.velocity = transform.forward * speed;
+	}
+	void OnCollisionEnter(){
+		SpawnAnimal ();
+	}
+	void SpawnAnimal(){
+		int type = Random.Range (0, 5);
+		GameObject _animal = Instantiate (animals[type]);
+		_animal.transform.position = transform.position;
+		_animal.SetActive (true);
+		Destroy (this.gameObject);
+	}
+
+}
