d2251bc6f25a84037c2835cf0feb268096e09d5f
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2018-02-05 18:37:03 -0700

    Moved SO folder, added beginnings of the horde system.

---
+++ UnityMasterProj/Assets/Scripts/ScriptableObjects/SO_EnemySmall.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Small Enemy", menuName = "Enemies/Small")]
+public class SO_EnemySmall : ScriptableObject 
+{
+    // Array of locations from which hordes (not individuals) will spawn.
+    public Vector3[] spawnPoints;
+}
