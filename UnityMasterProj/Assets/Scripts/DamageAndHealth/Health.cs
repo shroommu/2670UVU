360e1be2ae2e4c1777b2010326644e69891647f5
Fritz Dittmer <fdittmer9762@gmail.com>
2018-02-12 13:39:00 -0700

    Physics is hard
    Rigidbody is working
    **CC physics is difficult, disables the player movement and does not move after initial impact**
    taking a breather then adding real math

--- UnityMasterProj/Assets/Scripts/DamageAndHealth/Health.cs
+++ UnityMasterProj/Assets/Scripts/DamageAndHealth/Health.cs
@@ -8,9 +8,32 @@ public class Health : MonoBehaviour {
 
 	public Element currentElement;
 
-	public bool isMoveable;
-	public bool useRB;
+	public bool isMoveable = false;
 	[HideInInspector] public Vector3 appliedForce;
+
+	[HideInInspector] public Rigidbody RB;
+	public CharacterController CC;
+	public bool usesCC;
+
+	void Start(){
+		Setup ();
+	}
+
+	void Setup(){
+		RB = this.gameObject.GetComponent<Rigidbody>();
+		CC = this.gameObject.GetComponent<CharacterController>();
+		if (CC != null) {
+			isMoveable = true;
+			usesCC = true;
+			print ("I have a character Controller");
+		}
+		if (RB != null) {
+			CC = null;
+			isMoveable = true;
+			usesCC = false;
+			print ("I have a RB");
+		}
+	}
 
 	public void TakeDamage(int _dam, int _kBForce, Element damElement, Vector3 _dir ) {					//takes the damage, knockback force, element, and force direction
 		if (damElement != null) {															//if the damager has an element assigned to it
@@ -25,17 +48,39 @@ public class Health : MonoBehaviour {
 				}
 			}
 		} else { currentHealth += _dam; }													//if there are no elements just apply damage normally
-		print("New Health: " + _dir.x);
+
+		_dir = calculateForce (_dir, currentHealth, _kBForce);
 		if(isMoveable){
-			//ApplyForce (currenthealth * .01 * _dir)
+			if (usesCC) {
+				_dir = (calculateForce (_dir, Time.deltaTime, 1f));
+				StartCoroutine (ApplyForceCC (_dir));
+			} else { AddForce( _dir, RB); }
 		}
 	}
 
-	Vector3 calculateForce(Vector3 _forceVec){
-		_forceVec.x *= currentHealth * .01f;
-		_forceVec.y *= currentHealth * .01f;
-		_forceVec.z *= currentHealth * .01f;
+	Vector3 calculateForce(Vector3 _forceVec, float num, float num2){
+		_forceVec.x *= num2 * num;
+		_forceVec.y *= num2 * num;
+		_forceVec.z *= num2 * num;
 		return _forceVec;
+	}
+
+	public void AddForce(Vector3 _force, Rigidbody _RB){
+		_RB.AddForce (_force, ForceMode.Impulse);
+	}
+
+	public Vector3 AddForce(Vector3 _force, CharacterController _CC){
+		_CC.Move(calculateForce(_force, 1f, Time.deltaTime));
+		return calculateForce(_force, Time.deltaTime, .7f);
+	}
+
+	IEnumerator ApplyForceCC(Vector3 _dir){
+		Vector3 _force = AddForce (_dir, CC);
+		for(float i = 0f; i < 5f ; i += Time.deltaTime) {
+			yield return null;
+			print (Time.time);
+			_force = AddForce (_force, CC);
+		}
 	}
 
 }
